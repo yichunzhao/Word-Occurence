acf9a66cffe76fb3e26766652e9a3382e98dbe53
yichunzhao <zhaoyichun@hotmail.com>
2020-10-03 20:30:38 +0200

    refactoring

--- README.md
+++ README.md
@@ -8,3 +8,24 @@ Unit test
 ## Result
 ![image](https://user-images.githubusercontent.com/17804600/31588204-ecf85c0c-b1ee-11e7-95c1-befb78076a73.png)
 
+Using the supplied text file (tempest.txt) attached, English locale and treating hyphen and apostrophe as part of a word, it should give the following output:
+
+and (514)
+
+the (513)
+
+i (446)
+
+to (324)
+
+a (310)
+
+of (295)
+
+my (288)
+
+you (211)
+
+that (188)
+
+this (185) 
--- src/uniquewordintext/UniqueWordInText.java
+++ src/uniquewordintext/UniqueWordInText.java
@@ -6,24 +6,22 @@
 package uniquewordintext;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+
 /**
- *
  * @author YNZ
  */
 public class UniqueWordInText {
@@ -33,47 +31,80 @@ public class UniqueWordInText {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException, InterruptedException {
         Instant start = Instant.now();
 
-        Map<String, Long> top10 = wordOccurrence();
+        List<Map.Entry<String, Integer>> top10 = wordOccurrence();
 
         Instant end = Instant.now();
-
         System.out.println("Top 10 word: " + top10.toString());
         System.out.println("Time cost  :" + Duration.between(start, end).toMillis());
+
+        printOutAnotherWordOccurrence();
     }
 
-    public static Map<String, Long> wordOccurrence() throws IOException {
+    private static void printOutAnotherWordOccurrence() throws IOException {
+        System.out.println("++++++++++++++ another method ++++++++++++++++++++");
+        Instant start = Instant.now();
+        List<Map.Entry<Integer, String>> top10 = anotherWordOccurrence();
 
-        String str = new String(Files.readAllBytes(Paths.get("tempest.txt")), StandardCharsets.UTF_8);
+        Instant end = Instant.now();
+        System.out.println("Top 10 word: " + top10.toString());
+        System.out.println("Time cost  :" + Duration.between(start, end).toMillis());
+        System.out.println("++++++++++++++ another method +++++++++++++++++++++");
+    }
 
-        List<String> wordsList = Stream.of(str.toLowerCase().split(pattern))
+    public static List<Map.Entry<String, Integer>> wordOccurrence() throws IOException {
+
+        String str = Files.readString(Paths.get("tempest.txt"));
+
+        List<String> wordsList = Stream.of(str.toLowerCase(Locale.forLanguageTag("en")).split(pattern))
                 .collect(toList());
 
-        //all unique words
-        Set<String> wordsSet = wordsList.stream().distinct()
-                .collect(toSet());
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+        wordsList.forEach(w -> {
+            if (wordCountMap.containsKey(w)) {
+                int wordCount = wordCountMap.get(w);
+                wordCount++;
+                wordCountMap.put(w, wordCount);
+            } else {
+                wordCountMap.put(w, 1);
+            }
+        });
 
-        //counting each word
-        Map<String, Long> wordNumber = new HashMap<>();
-        wordsSet.forEach(word -> wordNumber.put(word, wordsList.stream()
-                .filter(w -> w.equals(word)).count()));
+        return wordCountMap.entrySet().stream().sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                .limit(10).collect(toList());
+    }
 
-        //all word occurrences
-        Map<String, Long> result = wordNumber.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (o, n) -> o, LinkedHashMap::new));  //o: existing key value; n: new value. 
+    public static List<Map.Entry<Integer, String>> anotherWordOccurrence() throws IOException {
 
-        //top 10 
-        Map<String, Long> top10 = result.entrySet().stream().limit(10)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (o, n) -> o, LinkedHashMap::new));
+        String str = Files.readString(Paths.get("tempest.txt"));
 
-        return top10;
+        List<String> wordsList = Stream.of(str.toLowerCase(Locale.forLanguageTag("en")).split(pattern))
+                .collect(toList());
+
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+        wordsList.forEach(w -> {
+            if (wordCountMap.containsKey(w)) {
+                int wordCount = wordCountMap.get(w);
+                wordCount++;
+                wordCountMap.put(w, wordCount);
+            } else {
+                wordCountMap.put(w, 1);
+            }
+        });
+
+        NavigableMap<Integer, String> sortedCountWordMap = wordCountMap.entrySet().stream().collect(Collectors.toMap(
+                entry -> entry.getValue(),
+                entry -> entry.getKey(),
+                (o, n) -> n,
+                TreeMap::new)
+        );
+
+        return sortedCountWordMap.descendingMap().entrySet()
+                .stream().limit(10).collect(toList());
 
     }
+
 
 }
